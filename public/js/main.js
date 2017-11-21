37465ed2bc2cedb0ebab85a66731908138b0c4fc
tak4822 <tkc4822@gmail.com>
2017-11-21 01:49:17 -0800

    complete requirement

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,8 @@
 $(function(){
-    //navbar
-    // $('.nav-has-sub').hover(function(){
-    //     $(this).find('.subNav').show("down", "swing", 800);
-    // },function(){
-    //     if(!$(this).hasClass('active')){
-    //         $(this).find('.subNav').toggle( "up",  "swing", 800);
-    //     }
-    // });
-    //fancy box
-    // $().fancybox({
-    //     selector : '[data-fancybox="images"]',
-    //     loop     : true,
-    //     helpers		: {
-    //         title	: { type : 'outside' }
-    //     },
-    //     tpl: {
-    //         wrap     : '<div class="fancybox-wrap" tabIndex="-1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"></div></div></div></div>',
-    //         image    : '<img class="fancybox-image" src="{href}" alt="" />',
-    //         iframe   : '<iframe id="fancybox-frame{rnd}" name="fancybox-frame{rnd}" class="fancybox-iframe" frameborder="0" vspace="0" hspace="0"' + ($.browser.msie ? ' allowtransparency="true"' : '') + '></iframe>',
-    //         error    : '<p class="fancybox-error">The requested content cannot be loaded.<br/>Please try again later.</p>',
-    //         closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"></a>',
-    //         next     : '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
-    //         prev     : '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
-    //     }
-    // });
     $().fancybox({
         selector : '[data-fancybox="images"]',
         loop     : true
     });
-
     //drawings show
     $('.chara-wrap').each(function(i, el){
         $(el).css({
@@ -41,9 +15,7 @@ $(function(){
                 'transform': 'translateY(0px)'
             });
         },300 + ( i * 140 ));
-
     });
-
 });
 
 //news button
@@ -51,4 +23,13 @@ function news(){
     $('.news-btn').click(function(){
         $('html, body').animate({'scrollTop': $('#news').offset().top}, 1000);
     });
+}
+
+function emailFilter(){
+    const f = "animationbooks";
+    const arr = "@";
+    const g = "gmail";
+    const dot = ".";
+    const c = "com";
+    document.getElementsById("emailText").href = "mail" + "to:" + f + arr + g + dot + c;
 }
